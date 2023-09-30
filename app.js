10aea3f4397244a5d40b3f090386c3ca5416bd91
Brett Connolly <brett84c@gmail.com>
2023-09-29 21:18:14 -0400

    update

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.set("port", process.env.PORT || 3000);
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
-app.use(express.json({ limit: "50mb" }));
-app.use(express.static("public"));
-
-module.exports = app;
